cec201552adbb81ee1d12620ecd3c06ae57d5dd0
ferferzada <moiseszandonai1234@gmail.com>
2023-07-24 17:58:56 -0300

    nuevo2

--- push.js
+++ push.js
@@ -28,3 +28,24 @@ async function sendNotificationAsync() {
         alert("Notification permissions denied or there was an error.");
     }
 }
+let button = document.getElementById("notification-button");
+
+button.addEventListener("click", function() {
+    // Verifica se o navegador suporta o Notification API
+    if (!("Notification" in window)) {
+        alert("Este navegador não suporta notificações de sistema.");
+    } 
+    // Verifica a permissão atual
+    else if (Notification.permission === "granted") {
+        alert("Você já habilitou as notificações!");
+    } 
+    // Solicita a permissão se ainda não foi solicitada ou negada
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+            // Se o usuário aceitou a permissão, nós configuramos a notificação
+            if (permission === "granted") {
+                alert("Notificações habilitadas!");
+            }
+        });
+    }
+});
